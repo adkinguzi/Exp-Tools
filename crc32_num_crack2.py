f94dbee10147bf09898d39b8256b51d983630781
adkinguzi <31617622+adkinguzi@users.noreply.github.com>
2020-08-27 10:42:03 +0800

    crc32_num_crack.py python3版本

---
+++ crc32_num_crack2.py
@@ -0,0 +1,18 @@
+from zlib import crc32
+from pip._vendor.distlib.compat import raw_input
+import random
+
+
+char='0123456789'
+def crc32_f(data):
+      return hex(crc32(data)&0xffffffff)[2:10]
+length=int(input('length:'),10)
+crc32_=raw_input('crc32:').lower()
+while True:
+     text=''
+     for i in range(length):
+         text+=char[random.randint(0,len(char)-1)]
+         print(i)
+     if crc32_f(text.encode())==crc32_:
+         raw_input('find it:'+text)
+         exit
